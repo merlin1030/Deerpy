39111e444b17bc30f653b2eba7421ffbe003db1a
Anthony Gadea <nightlycraow@gmail.com>
2022-02-16 10:16:01 -0400

    fix

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,15 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  
+
   uma = false;
 
   thumbnails = [
-    { title: 'Prueba1', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum dicta laborum tempore quo doloribus reiciendis totam, magni iusto ut quos velit eaque, aut perferendis quae optio recusandae architecto tempora nemo?', img: 'https://via.placeholder.com/365x200.png'  },
-    { title: 'Prueba2', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum dicta laborum tempore quo doloribus reiciendis totam, magni iusto ut quos velit eaque, aut perferendis quae optio recusandae architecto tempora nemo?', img: 'https://via.placeholder.com/365x200.png'  },
-    { title: 'Prueba3', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum dicta laborum tempore quo doloribus reiciendis totam, magni iusto ut quos velit eaque, aut perferendis quae optio recusandae architecto tempora nemo?', img: 'https://via.placeholder.com/365x200.png'  }
+    { title: 'Prueba1', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum dicta laborum tempore quo doloribus reiciendis totam, magni iusto ut quos velit eaque, aut perferendis quae optio recusandae architecto tempora nemo?', img: 'https://via.placeholder.com/365x200.png' },
+    { title: 'Prueba2', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum dicta laborum tempore quo doloribus reiciendis totam, magni iusto ut quos velit eaque, aut perferendis quae optio recusandae architecto tempora nemo?', img: 'https://via.placeholder.com/365x200.png' },
+    { title: 'Prueba3', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum dicta laborum tempore quo doloribus reiciendis totam, magni iusto ut quos velit eaque, aut perferendis quae optio recusandae architecto tempora nemo?', img: 'https://via.placeholder.com/365x200.png' }
   ];
-// metodos
+  // metodos
   constructor() { }
 
   toggleCards() {
